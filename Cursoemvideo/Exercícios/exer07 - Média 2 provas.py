bfb80ee06d7819fabeb89731426bce7ec0a7cc3e
Vith-MCB <vitorriblacerda@gmail.com>
2021-06-29 18:38:46 -0300

    Create exer07 - Média 2 provas.py

---
+++ Cursoemvideo/Exercícios/exer07 - Média 2 provas.py
@@ -0,0 +1,5 @@
+name = input('Nome do aluno: ')
+n1 = float(input('Nota primeira prova: '))
+n2 = float(input('Nota segunda prova: '))
+med = (n1+n2)/2
+print('A média do aluno {} foi de: {}'.format(name,med))
