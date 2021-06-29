ac04bb7641bac84c87592dc7140b35f551fef714
Vith-MCB <vitorriblacerda@gmail.com>
2021-06-29 19:20:31 -0300

    Update exer11 - L de tinta.py

--- Cursoemvideo/Exercícios/exer11 - L de tinta.py
+++ Cursoemvideo/Exercícios/exer11 - L de tinta.py
@@ -1,5 +1,6 @@
 h = float(input('Qual a altura da parede: '))
 l = float(input('Qual a largura: '))
+tint = float(input('Quantidade de m. quadrados por litro da tinta: '))
 print('Tamanho da parede em m^2: {}'.format(h*l))
-qt = (h*l)/2
+qt = (h*l)/tint
 print('Quntidade de tinta necessária: {:.1f}L'.format(qt))
