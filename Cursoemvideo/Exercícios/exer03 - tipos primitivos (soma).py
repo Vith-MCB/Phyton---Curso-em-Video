f60d4e9bfe2d35720d5366a57365139d3d430da1
Vith-MCB <vitorriblacerda@gmail.com>
2021-06-29 18:01:52 -0300

    Update exer03 - tipos primitivos (soma).py

--- Cursoemvideo/Exercícios/exer03 - tipos primitivos (soma).py
+++ Cursoemvideo/Exercícios/exer03 - tipos primitivos (soma).py
@@ -2,3 +2,4 @@ n1 = int(input('Digite um número: '))
 n2 = int(input('Digite um segundo número: '))
 som = n1+n2
 print('A soma vale: {}'.format(som))
+print('A média entre os números é: {}'.format(som/2))
