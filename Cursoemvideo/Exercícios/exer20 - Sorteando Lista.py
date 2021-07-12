44ffa619c7e8e28b591d662fa4208451fbeca159
Vith-MCB <vitorriblacerda@gmail.com>
2021-07-12 18:22:31 -0300

    Update exer20 - Sorteando Lista.py

--- Cursoemvideo/Exercícios/exer20 - Sorteando Lista.py
+++ Cursoemvideo/Exercícios/exer20 - Sorteando Lista.py
@@ -1,8 +1,8 @@
 import random
-p1 = input('Primeiro aluno: ')
-p2 = input('Segundo aluno: ')
-p3 = input('Terceiro aluno: ')
-p4 = input('Quarto aluno: ')
+p1 = str(input('Primeiro aluno: '))
+p2 = str(input('Segundo aluno: '))
+p3 = str(input('Terceiro aluno: '))
+p4 = str(input('Quarto aluno: '))
 list = [p1, p2, p3, p4]
 esc = random.sample(list, 4)
 print('A ordem de apresentação será: {}'.format(esc))
