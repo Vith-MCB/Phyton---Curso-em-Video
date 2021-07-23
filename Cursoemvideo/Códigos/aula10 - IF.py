b4fc1ea6162ebde988db3c0351d39d91decbcc72
Vith-MCB <vitorriblacerda@gmail.com>
2021-07-23 12:14:31 -0300

    Update aula10 - IF.py

--- Cursoemvideo/Códigos/aula10 - IF.py
+++ Cursoemvideo/Códigos/aula10 - IF.py
@@ -1,6 +1,10 @@
-m = 5
+m = 11
 if m <= 5:
     sit = ('Reprovado')
 else:
     sit = ('Aprovado')
 print('Você está: {}'.format(sit))
+if m in range(6,10):
+    print('Meus parabéns!')
+elif m > 10:
+    print('Que notão!!')
