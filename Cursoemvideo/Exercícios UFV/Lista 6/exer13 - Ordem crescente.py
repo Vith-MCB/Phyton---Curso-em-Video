fe3425c88be64e354e0524e402defdfd3004128f
Vith-MCB <vitorriblacerda@gmail.com>
2021-08-26 20:23:01 -0300

    .

--- Cursoemvideo/Exercícios UFV/Lista 6/exer13 - Ordem crescente.py
+++ Cursoemvideo/Exercícios UFV/Lista 6/exer13 - Ordem crescente.py
@@ -1,8 +1,2 @@
 vet = [int(input()) for i in range(5)]
-guarda = 0
-for i in range(5):
-    if vet[i] > vet[i+1]:
-        guarda = vet[i]
-        vet[i] = vet[i+1]
-        vet[i+1] = guarda
-print(vet)
+print(sorted(vet))
