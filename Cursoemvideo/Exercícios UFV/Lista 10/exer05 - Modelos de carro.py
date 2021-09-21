78f92c59c553f0ae479b5017f3f57f2597b14081
Vith-MCB <vitorriblacerda@gmail.com>
2021-09-21 19:50:51 -0300

    x

--- Cursoemvideo/Exercícios UFV/Lista 10/exer05 - Modelos de carro.py
+++ Cursoemvideo/Exercícios UFV/Lista 10/exer05 - Modelos de carro.py
@@ -6,8 +6,10 @@ for i in range(qtcar):
     car, km = kmc
     t = [str(car), int(km)]
     carkm.append(t)
-print(carkm)
 for j in range(qtcar):
     if maior < carkm[j][1]:
         maior = carkm[j][1]
-print(maior)
+        carrokm = carkm[j]
+print(carrokm)
+qtgas = (1000/maior) * 3.5
+print(qtgas)
